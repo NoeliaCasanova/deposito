e49c90c5bb6975082b295f856716eecaa4db02cd
Noelia <Noelia@Atenea>
2017-03-17 13:52:48 +0100

    Segundo commit

--- src/dam_ed04_actividad/CCuenta.java
+++ src/dam_ed04_actividad/CCuenta.java
@@ -1,82 +1,151 @@
 package dam_ed04_actividad;
 
+/**
+ *
+ * @author Noelia
+ */
 public class CCuenta {
 
+    /**
+     *
+     */
     protected String nombre;
+
+    /**
+     *
+     */
     protected String cuenta;
+
+    /**
+     *
+     */
     protected double saldo;
+
+    /**
+     *
+     */
     protected double tipoInterés;
 
-
+    /**
+     *
+     */
     public CCuenta() {
     
     }
 
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {    
         nombre=nom;
         cuenta=cue;
         saldo=sal;
     }
 
+    /**
+     *
+     * @param nom
+     */
     public void asignarNombre(String nom) {    
         setNombre(nom);
     }
 
+    /**
+     *
+     * @return
+     */
     public String obtenerNombre() {    
         return getNombre();
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double estado() {    
         return saldo;
     }
 
-
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception {    
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(saldo+cantidad);
     }
 
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception {    
-        if (cantidad <=0)
+        if (cantidad<=0)
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()<cantidad)
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(saldo-cantidad);
     }
 
-
+    /**
+     *
+     * @return
+     */
     public String obtenerCuenta() {    
         return cuenta;
     }
 
-
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre=nombre;
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     *
+     * @param tipoInterés
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés=tipoInterés;
     }
 
-    
+    /**
+     *
+     * @param cuenta
+     */
     public void setCuenta(String cuenta) {
         this.cuenta=cuenta;
     }
 
-    
+    /**
+     *
+     * @param saldo
+     */
     public void setSaldo(double saldo) {
         this.saldo=saldo;
     }
